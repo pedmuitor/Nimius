a4cc706c108313f916e4413f2b6e06ff97271ee4
pedmuitor <pedmuitor@gmail.com>
2011-10-03 23:15:41 +0200

    [NEW] Proyect NimEngineTest for testing

---
+++ NimEngineTest/src/NimEngineTestAppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  NimEngineTestAppDelegate.h
+//  NimEngineTest
+//
+//  Created by Pedro Muiños Tortajada on 02/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+@interface NimEngineTestAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ NimEngineTest/src/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NimEngineTest
+//
+//  Created by Pedro Muiños Tortajada on 02/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
